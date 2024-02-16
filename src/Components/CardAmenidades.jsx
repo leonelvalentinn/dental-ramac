993671a36574f54b191303098eb5bc09cf5b7884
leonelvalentinn <leonelvalentinn@gmail.com>
2024-02-15 19:20:00 -0600

    Remove Promotion

--- src/Components/CardAmenidades.jsx
+++ src/Components/CardAmenidades.jsx
@@ -110,14 +110,14 @@ const CardAmenidades = () => {
   };
   return (
     <div className="card-amenidades">
-      <h2>Promociones exclusivas para ti</h2>
+      {/*<h2>Promociones exclusivas para ti</h2>
       <div className="container-card-amenidades">
         <div className="item-promo">
           <img src={promo2} alt="Promoción 1" />
           <a href="https://wa.me/525544754479?text=Buenas tardes, solicito información sobre la limpieza dental" className="btn-header-contacto">Agenda tu cita</a>
         </div>
       </div>
-      {/*<p className="text-desc">
+      <p className="text-desc">
         Pon el cursor sobre la imagen para ver más información
       </p>
       <div className="container-card-amenidades">
