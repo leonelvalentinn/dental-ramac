619c8e2fff856698a94d65004368157db3513c16
leonelvalentinn <leonelvalentinn@gmail.com>
2024-02-23 17:59:44 -0600

    Section Dentist

--- src/Components/Amenidades.jsx
+++ src/Components/Amenidades.jsx
@@ -1,3 +1,6 @@
+// Import Swiper styles
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -10,10 +13,19 @@ import area4 from "../assets/edgar.png";
 import area5 from "../assets/liliana.png";
 import alejandro from "../assets/alejandro.png";
 import emiliano from "../assets/emiliano.png";
+import nora from "../assets/nora.png";
+import rodolfo from "../assets/rodolfo.png";
+import azul from "../assets/azul.png";
+import leon from "../assets/leon.png";
+import valeria from "../assets/valeria.png";
+import martha from "../assets/martha.png"
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper  } from 'swiper/react';
+// import Swiper core and required modules
+import { Navigation, Pagination } from 'swiper';
 // Import Swiper styles
 import 'swiper/css';
+
 
 const Amenidades = () => {
   const [visibleA, setVisibleA] = useState(false);
@@ -28,15 +40,23 @@ const Amenidades = () => {
   const [visibleJ, setVisibleJ] = useState(false);
   const [visibleK, setVisibleK] = useState(false);
   const [visibleL, setVisibleL] = useState(false);
+  const swiper = useSwiper();
   return (
     <div className="amenidades">
       <h2>Nuestro Equipo</h2>
       <div className="container-swiper">
         <Swiper
+          modules={[Navigation, Pagination]}
           spaceBetween={30}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
+          slidesPerView={1}
+          navigation={{
+            nextEl: '.review-swiper-button-next',
+            prevEl: '.review-swiper-button-prev',
+          }}
+          loop
+          breakpoints={{768: {slidesPerView:3}}}
           onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide>
             <div className="item-amenidades">
@@ -164,17 +184,17 @@ const Amenidades = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="item-amenidades">
-              <img src={area5} alt="" />
-              <h2>Dr. Joan Kenedy Mayo</h2>
+              <img src={nora} alt="Dra. Nora" />
+              <h2>Nora E. Castillo Pérez</h2>
               <CButton onClick={() => setVisibleG(!visibleG)}>Ver más</CButton>
               <CCollapse visible={visibleG}>
                 <CCard className="mt-3">
                   <CCardBody>
                     <ul className="list-details">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                      <li>Facultad de odontología UNAM</li>
+                      <li>Lic. en Cirujano Dentista <strong>Ced.Prof.5258208</strong></li>
+                      <li>División de estudios de posgrado UNAM</li>
+                      <li>Especialidad en ortodoncia <strong>Ced.Prof. 9627255</strong></li>
                     </ul>
                   </CCardBody>
                 </CCard>
@@ -183,17 +203,17 @@ const Amenidades = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="item-amenidades">
-              <img src={area5} alt="" />
-              <h2>Dr. Joan Kenedy Mayo</h2>
+              <img src={rodolfo} alt="Rodolfo" />
+              <h2>Rodolfo Uribe Castillo</h2>
               <CButton onClick={() => setVisibleH(!visibleH)}>Ver más</CButton>
               <CCollapse visible={visibleH}>
                 <CCard className="mt-3">
                   <CCardBody>
                     <ul className="list-details">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                      <li>Universidad del Magdalena - Colombia</li>
+                      <li>Odontólogo <strong>Ced. Prof. 1.082.985.066</strong></li>
+                      <li>División de estudios de posgrado UNAM</li>
+                      <li>Ortodoncia y ortopedia maxilofacial</li>
                     </ul>
                   </CCardBody>
                 </CCard>
@@ -202,17 +222,17 @@ const Amenidades = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="item-amenidades">
-              <img src={area5} alt="" />
-              <h2>Dr. Joan Kenedy Mayo</h2>
+              <img src={azul} alt="Dra. Azul" />
+              <h2>Azul M. González Gómez</h2>
               <CButton onClick={() => setVisibleI(!visibleI)}>Ver más</CButton>
               <CCollapse visible={visibleI}>
                 <CCard className="mt-3">
                   <CCardBody>
                     <ul className="list-details">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                      <li>Facultad de odontología UNAM</li>
+                      <li>Lic. en Cirujano Dentista <strong>Ced. Prof.10440497</strong></li>
+                      <li>División de estudios de posgrado UNAM</li>
+                      <li>Especialidad de Endodoncia</li>
                     </ul>
                   </CCardBody>
                 </CCard>
@@ -221,17 +241,16 @@ const Amenidades = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="item-amenidades">
-              <img src={area5} alt="" />
-              <h2>Dr. Joan Kenedy Mayo</h2>
+              <img src={leon} alt="León" />
+              <h2>León E. Moreno Villada</h2>
               <CButton onClick={() => setVisibleJ(!visibleJ)}>Ver más</CButton>
               <CCollapse visible={visibleJ}>
                 <CCard className="mt-3">
                   <CCardBody>
                     <ul className="list-details">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                      <li>CETIS 57 Ignacio Allende </li>
+                      <li>Laboratorista dental</li>
+                      <li><strong>Ced. Prof. 8624711</strong></li>
                     </ul>
                   </CCardBody>
                 </CCard>
@@ -240,17 +259,16 @@ const Amenidades = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="item-amenidades">
-              <img src={area5} alt="" />
-              <h2>Dr. Joan Kenedy Mayo</h2>
+              <img src={valeria} alt="Dra. Valeria" />
+              <h2>Valeria A. Esparza Rodríguez</h2>
               <CButton onClick={() => setVisibleK(!visibleK)}>Ver más</CButton>
               <CCollapse visible={visibleK}>
                 <CCard className="mt-3">
                   <CCardBody>
                     <ul className="list-details">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                      <li>Universidad Autónoma de Sinaloa </li>
+                      <li>Lic. en Cirujano Dentista <strong>Ced. Prof. 11343631</strong></li>
+                      <li>Técnico ceramista dental</li>
                     </ul>
                   </CCardBody>
                 </CCard>
@@ -259,17 +277,17 @@ const Amenidades = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="item-amenidades">
-              <img src={area5} alt="" />
-              <h2>Dr. Joan Kenedy Mayo</h2>
+              <img src={martha} alt="Dra. Martha" />
+              <h2>Martha G. Islas García</h2>
               <CButton onClick={() => setVisibleL(!visibleL)}>Ver más</CButton>
               <CCollapse visible={visibleL}>
                 <CCard className="mt-3">
                   <CCardBody>
                     <ul className="list-details">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
+                      <li>Facultad de Estudios Superiores Zaragoza. UNAM.</li>
+                      <li>Licenciatura en Cirujana Dentista. <strong>Ced. Prof. 6991412</strong></li>
+                      <li>Diplomado de Actualización Profesional en el Manejo de Tejidos Periimplantarios en Cirugía Bucal y Periodontal.</li>
+                      <li>Diplomado de Actualización Profesional en Terapia Periodontal</li>
                     </ul>
                   </CCardBody>
                 </CCard>
@@ -277,6 +295,8 @@ const Amenidades = () => {
             </div>
           </SwiperSlide>
         </Swiper>
+        <a className="icon-arrow review-swiper-button-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill='CurrentColor'><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/></svg></a>
+        <a className="icon-arrow review-swiper-button-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill='CurrentColor'><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/></svg></a>
       </div>
       <div className="container-amenidades">
         {/*<div className="item-amenidades">
