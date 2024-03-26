fd369609cb67ab603ce6ae10c34d80121bc117a5
leonelvalentinn <leonelvalentinn@gmail.com>
2024-03-26 12:39:19 -0600

    Contact Button Component

---
+++ src/Components/Button.jsx
@@ -0,0 +1,17 @@
+import "../Styles/botones.css"
+
+const Button = () => {
+
+  return (
+    <a
+      href="https://api.whatsapp.com/send?phone=525544754479"
+      target="_blank"
+      className="button-contact"
+      rel="noopener noreferrer"
+    >
+      Contacto
+    </a>
+  );
+} 
+
+export default Button
