e40feb9ab54c36c766bb03cf165bdba0050fb268
leonelvalentinn <leonelvalentinn@gmail.com>
2024-05-06 10:50:10 -0600

    Remove Promo

--- src/Components/CardAmenidades.jsx
+++ src/Components/CardAmenidades.jsx
@@ -1,6 +1,5 @@
 import '../Styles/card-amenidades.css'
 import promo1 from '../assets/promo-1.webp'
-import promo3 from '../assets/RAMAC.webp'
 import { useState, useEffect } from 'react'
 
 const CardAmenidades = () => {
@@ -43,10 +42,6 @@ const CardAmenidades = () => {
     <div className='card-amenidades'>
       <h2>Promociones exclusivas para ti</h2>
       <div className='container-card-amenidades'>
-        <div className={`item-promo ${hidePromo ? 'hidden' : ''}`}>
-          <img src={promo3} alt='Promoción paquete infantil' />
-          <a href='https://wa.me/525544754479?text=Buenas tardes, solicito información sobre la promoción del paquete infantil' className='btn-header-contacto'>Agenda tu cita</a>
-        </div>
         <div className='item-promo'>
           <img src={promo1} alt='Promoción 1' />
           <a href='https://wa.me/525544754479?text=Buenas tardes, solicito información sobre la limpieza dental' className='btn-header-contacto'>Agenda tu cita</a>
